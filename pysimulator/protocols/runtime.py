18f354f0c2363a04be69eae56800ea7e11f01d78
youngfreeFJS <471011042@qq.com>
2023-05-25 18:57:36 +0800

    feat(modal): init

---
+++ pysimulator/protocols/runtime.py
@@ -0,0 +1,22 @@
+class RunTime:
+    def __init__(self,
+                 bundle_path,
+                 bundle_version,
+                 runtime_root,
+                 identifier,
+                 version,
+                 is_available,
+                 name) -> None:
+        self.bundle_path = bundle_path
+        self.bundle_version = bundle_version
+        self.runtime_root = runtime_root
+        self.identifier = identifier
+        self.version = version
+        self.is_available = is_available
+        self.name = name
+
+
+
+
+
+
