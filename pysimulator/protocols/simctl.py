ecc10cea377b70805e55645319714b5fa7d74232
youngfreeFJS <471011042@qq.com>
2023-05-25 21:52:22 +0800

    feat(modal): modify list model to simctl model.

--- pysimulator/protocols/simctl.py
+++ pysimulator/protocols/simctl.py
@@ -15,7 +15,7 @@ class ListTypes:
 CMD = 'xcrun simctl list {0}'
 
 
-class List:
+class Simctl:
     def __init__(self) -> None:
         ...
 
@@ -23,11 +23,25 @@ class List:
         return Executor.shell(CMD.format('--help'))
 
     @staticmethod
-    def runtimes() -> str:
-        return Executor.shell(CMD.format(ListTypes.RUNTIMES))
+    def list(list_type: ListTypes) -> str:
+        return Executor.shell(CMD.format(ListTypes))
 
     @staticmethod
-    def runtimes_list() -> RunTimeList:
+    def list_runtimes() -> RunTimeList:
+        return [
+            RunTime(bundle_path=runtime_item.get('bundlePath'),
+                    bundle_version=runtime_item.get('bundleversion'),
+                    runtime_root=runtime_item.get('runtimeRoot'),
+                    identifier=runtime_item.get('identifier'),
+                    version=runtime_item.get('version'),
+                    is_available=runtime_item.get('isAvailable'),
+                    name=runtime_item.get('name')
+                    )
+            for runtime_item in Executor.shell(CMD.format(ListTypes.RUNTIMES) + ' --json').safe_json_stdout_data().get('runtimes')
+        ]
+    
+    @staticmethod
+    def list_devices() -> RunTimeList:
         return [
             RunTime(bundle_path=runtime_item.get('bundlePath'),
                     bundle_version=runtime_item.get('bundleversion'),
--- tests/test_protocols/test_list.py
+++ tests/test_protocols/test_list.py
@@ -1,9 +1,9 @@
-from pysimulator.protocols.commands.list import List
+from pysimulator.protocols.simctl import Simctl, ListTypes
 from pysimulator.protocols.runtime import RunTime, RunTimeList
 
 
 def test_list_cmd():
-    runtime_list = List.runtimes_list()
+    runtime_list = Simctl.list_runtimes()
     for runtime in runtime_list:
         assert isinstance(runtime, RunTime)
         assert runtime.name != None
