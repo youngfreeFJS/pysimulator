a81ab6f966a58a8bb5d12c982f1ff7a4c1e022a9
youngfreeFJS <471011042@qq.com>
2023-05-26 14:42:22 +0800

    feat(modal): add create device e2e tests.

--- tests/test_protocols/test_device.py
+++ tests/test_protocols/test_device.py
@@ -5,7 +5,7 @@ from pysimulator.protocols.simctl import Simctl
 def test_create_device():
     new_device = Device.create('demo01-device1',
                                device_type=Simctl.list_devicetypes().filter('iPhone X').first().identifier,
-                               runtime=Simctl.list_runtimes().filter('13.5').first().identifier
+                               runtime=Simctl.list_runtimes().filter('iOS').first().identifier
                                )
     assert len(new_device.udid) == 36
     assert new_device.name == 'demo01-device1'
--- tests/test_protocols/test_list.py
+++ tests/test_protocols/test_list.py
@@ -8,7 +8,7 @@ from pysimulator.protocols.device import Device, DeviceList
 def setup_new_device():
     new_device = Device.create('demo01-device1',
                                device_type=Simctl.list_devicetypes().filter('iPhone X').first().identifier,
-                               runtime=Simctl.list_runtimes().filter('13.5').first().identifier
+                               runtime=Simctl.list_runtimes().filter('iOS').first().identifier
                                )
     assert len(new_device.udid) == 36
     assert new_device.name == 'demo01-device1'
